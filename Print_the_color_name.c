7aed58f81fc62a1920d7ee784bd22b6d934706f5
lokesh855 <119839757+lokesh855@users.noreply.github.com>
2022-12-11 16:55:38 +0530

    #include<stdio.h> int main() {     char ch;     int z=-1;     scanf("%c",&ch);     if (ch=='V')     {         printf("Violet");         }         else if (ch=='I')         {             printf("Indigo");             }             else if(ch=='B')             {                 printf("Blue");                 }                 else if (ch=='G')                 {                     printf("Green");                     }                     else if(ch=='Y')                     {                         printf("Yellow");                         } else if (ch=='O')                         {                             printf("Orange");
                            }
                            else if(ch=='R')
                            {
                                printf("Red");
                                
                            } else 
                            {
                                printf("%d",z);
                                }
        
    }

---
+++ Print_the_color_name.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    int z=-1;
+    scanf("%c",&ch);
+    if (ch=='V')
+    {
+        printf("Violet");
+        }
+        else if (ch=='I')
+        {
+            printf("Indigo");
+            }
+            else if(ch=='B')
+            {
+                printf("Blue");
+                }
+                else if (ch=='G') 
+                {
+                    printf("Green");
+                    }
+                    else if(ch=='Y')
+                    {
+                        printf("Yellow");
+                        } else if (ch=='O')
+                        {
+                            printf("Orange");
+                            
+                        }
+                        else if(ch=='R')
+                        {
+                            printf("Red");
+                            
+                        } else 
+                        {
+                            printf("%d",z);
+                            }
+    
+}
