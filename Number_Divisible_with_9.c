909cdd289d373e57c11b9a428ef7201cb7c093a4
lokesh855 <119839757+lokesh855@users.noreply.github.com>
2022-12-12 18:34:09 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d",&a);     if(b=a%9==0)     {         printf("True");         }         else         {             printf("False"); }
    }

---
+++ Number_Divisible_with_9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    if(b=a%9==0)
+    {
+        printf("True");
+        }
+        else
+        {
+            printf("False"); }
+    
+}
