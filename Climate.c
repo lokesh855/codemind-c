f0a80c8fea1089f9d769a66bbf3a87dce3f58400
lokesh855 <119839757+lokesh855@users.noreply.github.com>
2022-12-12 18:35:27 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if(a>20)     {         printf("HOT");
        }
        else
        {
            printf("COLD");
            }
            return 0;
        
    }

---
+++ Climate.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>20)
+    {
+        printf("HOT"); 
+        
+    }
+    else
+    {
+        printf("COLD");
+        }
+        return 0;
+    
+}
